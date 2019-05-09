bcfbc7faca392a27316b4a005fd5fd813945bbed
skt-t1-byungi <tiniwz@gmail.com>
2019-05-10 06:56:10 +0900

    fix create-index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 // @create-index
 
+export { default as $ } from './$.js'
+export { default as $addClass } from './$addClass.js'
+export { default as $children } from './$children.js'
+export { default as $find } from './$find.js'
+export { default as $parseHTML } from './$parseHTML.js'
 export { default as DOM } from './DOM.js'
-export { default as addClass } from './$addClass.js/index.js'
 export { default as chain } from './chain.js'
 export { default as each } from './each.js'
-export { default as find } from './find.js'
+export { default as filter } from './filter.js'
 export { default as flatMap } from './flatMap.js'
+export { default as identity } from './identity.js'
+export { default as map } from './map.js'
 export { default as pipe } from './pipe.js'
 export { default as unique } from './unique.js'
